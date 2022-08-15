2db93c8bd3ef4cef3884c857dbf3d5c2e8f3c6f8
MatthewSavage <mat.savage@icloud.com>
2022-08-15 13:19:00 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Matthew Savage on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
